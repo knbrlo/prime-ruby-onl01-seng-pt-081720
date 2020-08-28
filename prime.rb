6d036c920292064a2f42fdb7612cdb6d776b3f34
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 22:26:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,36 @@
 # Add  code here!
 def prime?(number)
-  puts number
+
+  # is the number greater than 1
+  if number <= 1
+    return false
+  end
+
+  i = 2
+  
+  while i < number 
+    if number % 2 == 0
+      return false
+    else
+      return true
+    end
+  end
+
+  # divide the number by 2
+  # - if you get a whole number, if you do it is not a prime number
+  # - if you don't get a whole number, try dividing it by
+  # - - 3, 5, 7, 11
+
+  
+
+
+  # notes
+  # ----------
+  # how can we determine if a number is prime
+
+  # how do you create a range of numbers
+
+  # how can you create a range of numbers so that it can be iterated over?
+  # - i think it's done with the .to_a 
+  
 end
