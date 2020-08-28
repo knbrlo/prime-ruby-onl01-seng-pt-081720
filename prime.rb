e6e1cf4be0dfc59287416b23399d181eab9faf99
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 23:05:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,4 @@
-require 'pry'
-
 def prime?(number)
 
-  is_prime = true
-  for i in 2..number-1
-    if number % i == 0
-      is_prime = false
-    end
-  end
-  is_prime
-# notes
-  # ----------
-  # how can we determine if a number is prime
-
-  # how do you create a range of numbers
-
-  # how can you create a range of numbers so that it can be iterated over?
-  # - i think it's done with the .to_a
 
 end
